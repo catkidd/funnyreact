33273615edc56830da1bfdf0f2c496ce82762861
catkidd <catkidd770@gmail.com>
2023-01-25 09:06:28 +0545

    updated code

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+// console.log("hello");
+function changeContent() {
+    document.getElementById("hundred").style.fontSize = "22px";
+    document.getElementById("hundred").style.color = "red";
+    console.log("done");
+}
+
+function turnOn() {
+    document.getElementById("turn-on-off").src = "https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg";
+    console.log("done");
+}
+function turnOff() {
+    document.getElementById("turn-on-off").src = "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+    console.log("done");
+}
